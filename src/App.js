3e9d20e091a88b1bbd6e755ead67f3fb3e417041
Daniel Oram <danieloramnz@gmail.com>
2021-04-02 16:34:18 +0100

    create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './Navbar';
+
+// import Image from './static/img/oranges.jpg';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>
         Mars Rover Photos
       </h1>
+      
     </div>
   );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Mars Rover Photos</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
